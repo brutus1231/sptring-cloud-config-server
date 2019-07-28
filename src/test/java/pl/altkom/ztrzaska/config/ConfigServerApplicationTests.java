a1a37f16f6f1b3651d5a6b066fc57bf8ba4466a0
brutus1231 <ztrzaska2@gmail.com>
2019-07-28 11:34:58 +0200

    initial commit

--- src/test/java/pl/altkom/ztrzaska/config/ConfigServerApplicationTests.java
+++ src/test/java/pl/altkom/ztrzaska/config/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package pl.altkom.ztrzaska;
+package pl.altkom.ztrzaska.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SptringCloudConfigServerApplicationTests {
+public class ConfigServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
